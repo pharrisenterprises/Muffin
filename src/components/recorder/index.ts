1f4a4580cf062f41eef42901af0bb95ba085ed13
pharrisenterprises <pharrisenterprises@gmail.com>
2025-12-02 14:38:08 +0000

    feat(UI-010, UI-011, UI-012, UI-013): Add StepRow with badge display and menu items

---
+++ src/components/recorder/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Recorder Components Index
+ * Re-exports all recorder components.
+ */
+
+export { StepRow } from './StepRow';
+export type { StepRowProps } from './StepRow';
+
+export { StepRowMenu } from './StepRowMenu';
+export type { StepRowMenuProps } from './StepRowMenu';
+
+export { StepList } from './StepList';
+export type { StepListProps } from './StepList';
